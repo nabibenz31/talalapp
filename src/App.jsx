b99a574b25bb7bb0b4e6e7abf7aa51fca022dfb0
nabibenz31 <nabibenz31@gmail.com>
2024-04-20 02:16:59 +0100

    0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <>
       <div className="App bg-slate-200">
         <h1 className="text-3xl font-bold underline">Hello Talal!</h1>
+        <h1 className="text-3xl font-bold underline">From me to you</h1>
       </div>
     </>
   );
